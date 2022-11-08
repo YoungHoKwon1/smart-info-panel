4ea69931a65556e9f2f7959b724819ea28f62f84
Gunhee <ilove9952@naver.com>
2022-11-09 02:17:02 +0900

    teacher_info.dart error

--- lib/classinfo10.dart
+++ lib/classinfo10.dart
@@ -2,7 +2,6 @@ import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:fl_chart/fl_chart.dart';
 
 import 'api/infopanel.dart';
 
