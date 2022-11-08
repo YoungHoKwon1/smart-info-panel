2755471ebd318668eee93f55f60b492fa7aa9483
DESKTOP-PR39U5I\82106 <smmaxcho@naver.com>
2022-11-08 17:28:52 +0900

    create notice file

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:smart_info_panel/classinfo30.dart';
 import 'package:smart_info_panel/classinfo20.dart';
+import 'package:smart_info_panel/notice.dart';
 //import 'package:easy_localization/easy_localization.dart';
 
 import 'classinfo10.dart';
 import 'classinfo30.dart';
 import 'fl_graph_test.dart';
+import 'notice.dart';
 
 //maindart change test
 
@@ -58,7 +60,7 @@ class _SmartInfoPanelMainState extends State<SmartInfoPanelMain> {
           color: Colors.white,
           child: ElevatedButton(
             onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => ClassInfo20()));
+              Navigator.push(context, MaterialPageRoute(builder: (context) => Notice()));
               // Navigator.push(context, MaterialPageRoute(builder: (context) => ClassInfo10()));
              // Navigator.push(context, MaterialPageRoute(builder: (context) => ClassInfo30()));
               // Navigator.push(context, MaterialPageRoute(builder: (context) => GraphTest()));
